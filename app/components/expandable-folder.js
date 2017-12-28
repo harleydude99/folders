96317b70080ca2c54e4c681b72a41626fff41fa3
Harley Dude <harleydude99@gmail.com>
2017-12-28 14:22:12 -0700

    functional working for add files and folders, not component yet

--- app/components/expandable-folder.js
+++ app/components/expandable-folder.js
@@ -2,9 +2,28 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   isExpanded: false,
+  newFile: null,
+  newFolder: null,
+  store: Ember.inject.service(),
   actions:{
     toggleExpansion(){
       this.toggleProperty('isExpanded');
+    },
+    saveNewFolder(folder){
+      let toSaveObject = {
+        name:this.get('newFolder'),
+        parent: folder
+      };
+      this.get('store').createRecord('folder', toSaveObject).save();
+      this.set('newFolder', '');
+    },
+    saveNewFile(folder){
+      let toSaveObject = {
+        name:this.get('newFile'),
+        folder: folder
+      };
+      this.get('store').createRecord('file', toSaveObject).save();
+      this.set('newFile', '');
     }
   }
 });
--- app/models/folder.js
+++ app/models/folder.js
@@ -2,5 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   name: DS.attr('string'),
+  folders: DS.hasMany('folder', {inverse: 'parent'}),
+  parent: DS.belongsTo('folder', {inverse: 'folders'}),
   files: DS.hasMany()
 })
